07cef8899b1079411b9dac22fe4783637e48e7d0
shortykg <swtkaitlyn@gmail.com>
2019-04-12 18:40:09 -0700

    Finished Test

--- tests/testthat/test-NNetEarlyStoppingCV.R
+++ tests/testthat/test-NNetEarlyStoppingCV.R
@@ -8,24 +8,24 @@ context("test-NNetEarlyStoppingCV")
 
 
 test_that("NNetEarlyStoppingCV computes the right demensions", {
-  data(SAheart , package = "ElemStatLearn")
-  X.mat<-as.matrix(SAheart [1:50,-9])
-  y.vec<-SAheart [1:50, 9]
+  data(ozone, package="ElemStatLearn")
+  X.mat <- as.matrix(ozone[,-1])
+  y.vec <- as.vector(ozone[,1])
   max.iterations <- 100
   step.size <- .5
   n.hidden.units <- 2
-  res <- NNetEarlyStoppingCV(X.mat, y.vec, max.iterations, n.hidden.units)
+  result <- NNetEarlyStoppingCV(X.mat, y.vec, , max.iterations, step.size, n.hidden.units)
   
   expect_equal(length(res), 7)
 })
 
 test_that("NNetEarlyStoppingCV throws errors", {
-  data(SAheart , package = "ElemStatLearn")
-  X.mat<-c(SAheart [1:50,-9])
-  y.vec<-SAheart [1:50, 9]
+  data(ozone, package="ElemStatLearn")
+  X.mat <- as.matrix(ozone[,-1])
+  y.vec <- as.vector(ozone[,1])
   max.iterations <- 100
   step.size <- .5
   n.hidden.units <- 2
   
-  expect_error(NNetEarlyStoppingCV(X.mat, y.vec, fold.vec, max.iterations, n.hidden.units), "Feature matrix is not a matrix")
+  expect_error(NNetEarlyStoppingCV(X.mat, y.vec, , max.iterations, step.size, n.hidden.units), "Feature matrix is not a matrix")
 })
